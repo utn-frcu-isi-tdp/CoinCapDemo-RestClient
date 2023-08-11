ab5b8a8429a8212edf2fa69c02314602e9209842
etripodi <etripodi@gmail.com>
2023-08-11 18:01:12 -0300

    Update IApiClient.cs

--- CoinCapDemo/Facades/IApiClient.cs
+++ CoinCapDemo/Facades/IApiClient.cs
@@ -3,7 +3,7 @@
     internal interface IApiClient
     {
 
-        T Post<T>(string resourceUrl, object? body = null, Dictionary<string, string>? urlParams = null) where T : new();
+        T Post<T>(string resourceUrl, object? body1 = null, Dictionary<string, string>? urlParams = null) where T : new();
 
         T Get<T>(string resourceUrl, object? body = null, Dictionary<string, string>? urlParams = null) where T : new();
 
