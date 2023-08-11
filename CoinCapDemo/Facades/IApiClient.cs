5db0b0a9cff54ba7ee5876269bc5d0e9e0984ee1
Esteban Tripodi <etripodi@gmail.com>
2023-08-11 18:30:14 -0300

    12222

--- CoinCapDemo/Facades/IApiClient.cs
+++ CoinCapDemo/Facades/IApiClient.cs
@@ -5,7 +5,7 @@
 
         T Post<T>(string resourceUrl, object? body = null, Dictionary<string, string>? urlParameters = null) where T : new();
 
-        T Get<T>(string resourceUrl1, object? body = null, Dictionary<string, string>? urlParams = null) where T : new();
+        T Get<T>(string resourceUrl1, object? body1 = null, Dictionary<string, string>? urlParams = null) where T : new();
 
         T Patch<T>(string resourceUrl, object? body = null, Dictionary<string, string>? urlParams = null) where T : new();
 
